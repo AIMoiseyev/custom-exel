5134b28d3f64a48d908695adf541bb3642ca72b3
Alexander Moiseyev <daklaktakpak@yandex.ru>
2021-09-12 00:19:09 +0500

    finish base framework functional

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,18 @@
 module.exports = {
-  parser: '@babel/eslint-parser',
+  parser: "@babel/eslint-parser",
   parserOptions: {
     babelOptions: {
-      configFile: './babel.config.json',
+      configFile: "./babel.config.json",
     },
   },
   env: {
     browser: true,
     node: true,
     es6: true,
-
   },
-  extends: ['eslint:recommended', 'google'],
+  // extends: ["eslint:recommended", "google"],
   rules: {
-    'semi': 'off',
-    'comma-dangle': 'off'
-  }
+    semi: "off",
+    "comma-dangle": "off",
+  },
 };
