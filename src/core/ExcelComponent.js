5ca4254fdd2f5df2a623d7df285d2e5734523330
Alexander Moiseyev <daklaktakpak@yandex.ru>
2021-09-16 23:35:16 +0500

    finish base table logic

--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -4,17 +4,36 @@ export class ExcelComponent extends DomListener {
   constructor($root, options = {}) {
     super($root, options.listeners);
     this.name = options.name || "";
+    this.emitter = options.emitter;
+    this.unsubscribers = [];
+
+    this.prepare();
   }
+  // настраиваем компонент до init
+  prepare() {}
   //возвращает шаблон компонента
   toHTML() {
     return "";
   }
+  //уведомляем слушателей про событие event
+  $emit(event, ...agrs) {
+    this.emitter.emit(event, ...agrs);
+  }
+  // подписываемся на событие event
+  $on(event, fn) {
+    const unsub = this.emitter.subscribe(event, fn);
+    this.unsubscribers.push(unsub);
+  }
 
+  // инициализируем компонента
+  // добавляем DOM слушателей
   init() {
     this.initDOMListeners();
   }
-
+  // удаляем компонет
+  //чистим слушателей
   destroy() {
     this.removeDOMListeners();
+    this.unsubscribers.forEach((unsub) => unsub());
   }
 }
