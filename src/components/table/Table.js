3397b390c27e466a4c41f80a795e04fc908e9874
Alexander Moiseyev <daklaktakpak@yandex.ru>
2021-09-19 21:30:04 +0500

    finish redux and table logic

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -9,6 +9,9 @@ import {
   nextSelection,
 } from "@/components/table/table.functions";
 import { TableSelection } from "@/components/table/TableSelection";
+import * as actions from "@/redux/actions";
+import { defaultStyles } from "@/constants";
+import { parse } from "@core/parse";
 
 export class Table extends ExcelComponent {
   static className = "excel__table";
@@ -28,7 +31,7 @@ export class Table extends ExcelComponent {
   }
 
   toHTML() {
-    return createTable(20);
+    return createTable(20, this.store.getState());
   }
 
   prepare() {
@@ -42,22 +45,43 @@ export class Table extends ExcelComponent {
     this.selectCell($cell);
 
     this.$on("formula:input", (text) => {
-      this.selection.current.text(text);
+      this.selection.current.attr("data-value", text).text(parse(text));
+      this.updateTextInStore(text);
     });
 
     this.$on("formula:done", () => {
       this.selection.current.focus();
+    });
+    this.$on("toolbar:applyStyle", (style) => {
+      this.selection.applyStyle(style);
+      this.$dispatch(
+        actions.applyStyle({
+          value: style,
+          ids: this.selection.selectedIds,
+        })
+      );
     });
   }
 
   selectCell($cell) {
     this.selection.select($cell);
     this.$emit("table:select", $cell);
+    const styles = $cell.getStyles(Object.keys(defaultStyles));
+    this.$dispatch(actions.changeStyles(styles));
+  }
+
+  async resizeTable(event) {
+    try {
+      const data = await resizeHandler(this.$root, event);
+      this.$dispatch(actions.tableResize(data));
+    } catch (err) {
+      console.warn("Resize error", err.message);
+    }
   }
 
   onMousedown(event) {
     if (shouldResize(event)) {
-      resizeHandler(this.$root, event);
+      this.resizeTable(event);
     } else if (isCell(event)) {
       const $target = $(event.target);
       if (event.shiftKey) {
@@ -69,7 +93,7 @@ export class Table extends ExcelComponent {
         );
         this.selection.selectGroup($cells);
       } else {
-        this.selection.select($target);
+        this.selectCell($target);
       }
     }
   }
@@ -94,8 +118,19 @@ export class Table extends ExcelComponent {
     }
   }
 
+  updateTextInStore(value) {
+    this.$dispatch(
+      actions.changeText({
+        id: this.selection.current.id(),
+        value,
+      })
+    );
+  }
+
   onInput(event) {
-    this.$emit("table:input", $(event.target));
+    // this.$emit("table:input", $(event.target));
+    const text = $(event.target).text();
+    this.updateTextInStore(text);
   }
   // onMousemove() {
   //   console.log("mousemove");
--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -5,7 +5,10 @@ export class ExcelComponent extends DomListener {
     super($root, options.listeners);
     this.name = options.name || "";
     this.emitter = options.emitter;
+    this.subscribe = options.subscribe || [];
+    this.store = options.store;
     this.unsubscribers = [];
+    // this.storeSub = null;
 
     this.prepare();
   }
@@ -25,6 +28,20 @@ export class ExcelComponent extends DomListener {
     this.unsubscribers.push(unsub);
   }
 
+  $dispatch(action) {
+    this.store.dispatch(action);
+  }
+
+  // $subscribe(fn) {
+  //   this.storeSub = this.store.subscribe(fn);
+  // }
+
+  storeChanged() {}
+
+  isWatching(key) {
+    return this.subscribe.includes(key);
+  }
+
   // инициализируем компонента
   // добавляем DOM слушателей
   init() {
@@ -35,5 +52,6 @@ export class ExcelComponent extends DomListener {
   destroy() {
     this.removeDOMListeners();
     this.unsubscribers.forEach((unsub) => unsub());
+    // this.storeSub.unsubscribe();
   }
 }
