1e2816d671ef523b1284ddd4b08ff493cddbca88
Alexander Moiseyev <daklaktakpak@yandex.ru>
2021-09-21 22:19:33 +0500

    Create unit testing dev

--- webpack.config.js
+++ webpack.config.js
@@ -28,11 +28,6 @@ module.exports = (env, argv) => {
         filename: filename("css"),
       }),
       new webpack.DefinePlugin({
-        // PRODUCTION: JSON.stringify(true),
-        // VERSION: JSON.stringify("5fa3b9"),
-        // BROWSER_SUPPORTS_HTML5: true,
-        // TWO: "1+1",
-        // "typeof window": JSON.stringify("object"),
         "process.env.NODE_ENV": JSON.stringify(argv.mode),
       }),
     ];
